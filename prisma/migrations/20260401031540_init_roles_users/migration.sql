d8b41e88cd2c5ac6c94088317c2586492a7d0aca
dqadralamsah <dqadralamsah@gmail.com>
2026-04-01 10:52:30 +0700

    feat: complated database setup with schema & seeding

---
+++ prisma/migrations/20260401031540_init_roles_users/migration.sql
@@ -0,0 +1,36 @@
+-- CreateTable
+CREATE TABLE "roles" (
+    "id" UUID NOT NULL,
+    "role_code" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "roles_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" UUID NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "role_id" UUID NOT NULL,
+    "is_active" BOOLEAN NOT NULL DEFAULT true,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "roles_role_code_key" ON "roles"("role_code");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE INDEX "users_role_id_idx" ON "users"("role_id");
+
+-- AddForeignKey
+ALTER TABLE "users" ADD CONSTRAINT "users_role_id_fkey" FOREIGN KEY ("role_id") REFERENCES "roles"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ prisma/seed.ts
@@ -0,0 +1,80 @@
+import { PrismaClient, Prisma } from '../app/generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import * as bcrypt from 'bcrypt';
+import 'dotenv/config';
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const prisma = new PrismaClient({
+  adapter,
+});
+
+async function main() {
+  console.log('Sedang memulai proses seeding...');
+
+  // Data Role
+  const rolesData = [
+    { roleCode: 'ROLE-001', name: 'Super Administrator' },
+    { roleCode: 'ROLE-002', name: 'Purchasing Staff' },
+    { roleCode: 'ROLE-003', name: 'Warehouse Staff' },
+  ];
+
+  // Roles Seeding
+  console.log('Menyiapkan Roles...');
+
+  for (const r of rolesData) {
+    const result = await prisma.role.upsert({
+      where: { roleCode: r.roleCode },
+      update: {},
+      create: {
+        roleCode: r.roleCode,
+        name: r.name,
+      },
+    });
+
+    console.log(`Berhasil memuat data role: ${result.roleCode}`);
+  }
+
+  console.log('Proses seeding roles selesai!');
+
+  // Mengambil Role Super Administrator
+  const superAdminRole = await prisma.role.findUnique({
+    where: { roleCode: 'ROLE-001' },
+  });
+
+  if (!superAdminRole) {
+    throw new Error('Role ROLE-001 tidak ditemukan!');
+  }
+
+  // Users Seeding
+  console.log('Menyiapkan User Admin Pertama...');
+
+  const hashedPassword = await bcrypt.hash('Superadmin123', 10);
+
+  const superAdminUser = await prisma.user.upsert({
+    where: { email: 'superadmin@procurement.com' },
+    update: {},
+    create: {
+      email: 'superadmin@procurement.com',
+      name: 'Super Administrator',
+      password: hashedPassword,
+      roleId: superAdminRole.id,
+      isActive: true,
+    },
+  });
+
+  console.log(`Berhasil memuat data user: ${superAdminUser.email}`);
+  console.log('Seeding Selesai...');
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
